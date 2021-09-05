8dc90705ced98b46fabd974935eb08f86f853f7d
bergsans <bergclaesmagnus@gmail.com>
2021-09-05 18:51:39 +0200

    Collect function in index

---
+++ index.js
@@ -0,0 +1,11 @@
+const { figletFmtStr } = require("./lib/ascii-print");
+const { boxify } = require("./lib/boxify");
+const { logObj } = require("./lib/log-object");
+const { print } = require("./lib/print");
+
+module.exports = {
+  figletFmtStr,
+  boxify,
+  logObj,
+  print,
+};
