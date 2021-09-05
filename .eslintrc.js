00963c8962a2192ae10da15cddeb551b2fa002be
bergsans <bergclaesmagnus@gmail.com>
2021-09-05 20:44:50 +0200

    Add source code examples

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,10 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["airbnb-base"],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  rules: {
-    quotes: [2, "double"],
-  },
+  rules: {},
 };
