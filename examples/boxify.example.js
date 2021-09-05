52587beeecdd5fead4825e1564eab6e4ad5e6277
bergsans <bergclaesmagnus@gmail.com>
2021-09-05 20:12:00 +0200

    Add examples (boxify) and add fmtStr to lib

---
+++ examples/boxify.example.js
@@ -0,0 +1,7 @@
+const { boxify } = require("crude-dev-tools");
+
+const str = `
+Hello world!
+This is a box.
+    `;
+console.log(boxify(str));
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { figletFmtStr } = require("./lib/ascii-print");
 const { boxify } = require("./lib/boxify");
 const { logObj } = require("./lib/log-object");
-const { print } = require("./lib/print");
+const { print, fmtStr } = require("./lib/print");
 
 module.exports = {
   figletFmtStr,
   boxify,
   logObj,
   print,
+  fmtStr,
 };
